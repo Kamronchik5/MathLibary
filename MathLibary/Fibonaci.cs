f5f1475abf8c776d234f8fd5d46e2f400d6be04c
Xabibullayev Kamronbek <simplekux8082@icloud.com>
2024-04-29 23:52:16 +0500

    new project added

---
+++ MathLibary/Fibonaci.cs
@@ -0,0 +1,24 @@
+﻿namespace MathLibary
+{
+    internal class Fibonaci
+    {
+        public void Fibo(int x)
+        {
+            Console.WriteLine("Input the n");
+            string numbs = Console.ReadLine();
+            int n = Convert.ToInt32(numbs);
+            int[] numbers = new int[n];
+            numbers[0] = 0;
+            numbers[1] = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                numbers[i] = numbers[i - 1] + numbers[i - 2];
+            }
+            foreach (int num in numbers)
+            {
+                Console.Write(num + "  ");
+            }
+        }  
+    }
+}
--- MathLibary/Program.cs
+++ MathLibary/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using MathLibary;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-
+        Fibonaci fibo = new Fibonaci();
+        Console.WriteLine("Welcome to programm");
+        Console.WriteLine("Choose menu :");
+        Console.WriteLine("1-Fibonaci number");
+        Console.WriteLine("2-Prime number");
+        Console.WriteLine("3-Pifagor");
+        
     }
 }
